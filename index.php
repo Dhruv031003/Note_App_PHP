d9a437f3096a2791de834ea3a822066a1941d5cd
Dhruv031003 <dhruvdorbi03@gmail.com>
2022-12-01 23:53:26 +0530

    basic CRUD operations added

--- index.php
+++ index.php
@@ -4,15 +4,35 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "CRUD_APP";
-$alert= false;
+$insert = false;
+$update = false;
+$delete = false;
+
 $conn = mysqli_connect($servername, $username, $password, $database);
 
+if(isset($_GET['delete'])){
+    $Sno=$_GET['delete'];
+    $delete_query = "DELETE FROM `notes` WHERE `notes`.`s_no` = $Sno";
+    $delete_req = mysqli_query($conn, $delete_query);
+    $delete = "true";
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $insert_query = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description');";
-    $insert_req = mysqli_query($conn, $insert_query);
-    $alert = "true";
+    if(isset($_POST['Sno'])){
+        $Sno=$_POST['Sno'];
+        $titleEdit = $_POST['titleEdit'];
+        $descriptionEdit = $_POST['descriptionEdit'];
+        $update_query = "UPDATE `notes` SET `title` = '$titleEdit', `description` = '$descriptionEdit' WHERE `notes`.`s_no` = '$Sno'";
+        $update_req = mysqli_query($conn, $update_query);
+        $update = "true";
+    }
+    else{
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $insert_query = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description');";
+        $insert_req = mysqli_query($conn, $insert_query);
+        $insert = "true";
+    }
 }
 
 ?>
@@ -28,6 +48,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body>
+    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+  Launch demo modal
+</button> -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Note</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="index.php?update=true" method="POST">
+                        <input type="hidden" name="Sno" id="Sno">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Title</label>
+                            <input type="text" id="titleEdit" name="titleEdit" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Add note Description</label>
+                            <textarea type="text" id="descriptionEdit" name="descriptionEdit" class="form-control" rows="5"></textarea>
+                        </div>
+                        <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">CRUD App</a>
@@ -54,8 +106,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </nav>
     <?php
-    if ($alert) {
+    if ($insert) {
         echo '<div class="alert alert-success alert-dismissible fade show m-0" role="alert"><strong>Note added Successfully!!!!</strong><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+    }
+    if ($update) {
+        echo '<div class="alert alert-success alert-dismissible fade show m-0" role="alert"><strong>Note updated Successfully!!!!</strong><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+    }
+    if ($delete) {
+        echo '<div class="alert alert-success alert-dismissible fade show m-0" role="alert"><strong>Note deleted Successfully!!!!</strong><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
     }
     ?>
     <main>
@@ -93,7 +151,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 <th scope='row'>" . $sno . "</th>
                                 <td>" . $rows['title'] . "</td>
                                 <td>" . $rows['description'] . "</td>
-                                <td>@mdo</td>
+                                <td><button type='button' class='edit btn btn-primary m-2' id=".$rows['s_no'].">Edit</button><button type='button' class='delete btn btn-primary m-2' id=d".$rows['s_no'].">Delete</button></td>
                                 </tr>";
                             $sno++;
                         }
@@ -110,6 +168,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $(document).ready(function() {
             $('#myTable').DataTable();
         });
+        edits = document.getElementsByClassName('edit');
+        Array.from(edits).forEach((element) => {
+            element.addEventListener("click", (e) => {
+                tr = e.target.parentNode.parentNode;
+                title = tr.getElementsByTagName("td")[0].innerText
+                description = tr.getElementsByTagName("td")[1].innerText
+                console.log(title, description)
+                titleEdit.value = title
+                descriptionEdit.value = description;
+                Sno.value= e.target.id
+                console.log(Sno)
+                $('#editModal').modal('toggle')
+            })
+        })
+        deletes = document.getElementsByClassName('delete');
+        Array.from(deletes).forEach((element) => {
+            element.addEventListener("click", (e) => {
+                Sno=e.target.id.substr(1,);
+                if(confirm("Press a Button!")){
+                    window.location=`index.php?delete=${Sno}`    
+                }
+                else{
+                    console.log("no")
+                }
+            })
+        })
     </script>
 </body>
 
